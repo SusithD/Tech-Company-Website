7286659f233180a48bf8f46da5a0f9de5ffc6bbd
SusithD <iamsusithalwis@gmail.com>
2025-06-27 10:35:17 +0530

    Add CultureSection component as a placeholder for merged content in AchievementsSection

---
+++ components/CultureSection.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <!-- Culture content is now included in AchievementsSection -->
+  </div>
+</template>
+
+<script setup>
+// This component is a placeholder as culture content 
+// has been merged into AchievementsSection
+</script>
